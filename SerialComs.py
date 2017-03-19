d316fc5f6d15c57a8a1e36f1c0ca6b5a12fcf1ff
Jacob Anderson <jaanderson@fortlewis.edu>
2017-03-18 21:32:08 -0600

    Up date Serial coms with the beginning of serial port listing

--- SerialComs.py
+++ SerialComs.py
@@ -2,20 +2,39 @@
 
 import time
 import serial
+import glob
+#import numpy
+
+ports = glob.glob('/dev/ttyUSB*')
+
+coms = []
+
+for port in ports:
+    try:
+        s = serial.Serial(port)
+        s.close()
+        coms.append(port)
+
+            
+    except (OSError , serial.SerialException):
+        pass
+
+ports = tuple(coms)
 
 
+for i in range(0,len(ports)):
+    print(ports[i])
+
+# Open Gineric Serial Ports
+#serial1 = serial.Serial(None)
 
 # Open the serial port for nema sentances ----------------------------------------
 dataPort = serial.Serial("/dev/ttyUSB1",baudrate = 19200)
 
+
 # Open the serial port with the line counter ----------------------------------------
 lineCounter = serial.Serial("/dev/ttyUSB0",baudrate = 115200, timeout = 3.0)
 
-
-state = ''
-location = ''
-data = ''
-distance = ''
 
 print("Main loop")
 
@@ -23,9 +42,8 @@ while True:
 
     # Read nema data from ROV ------------------------------------------------
     if dataPort.inWaiting():
+        
         dataRecived = dataPort.readline()
-    elif lineCounter.inWaiting():
-        dataRecived = lineCounter.readline()
         
         if dataRecived[0:5] == bytes('$CARE'.encode('utf-8')):
             state = str(dataRecived[7:len(dataRecived)-2])
@@ -43,7 +61,7 @@ while True:
         
 
     # Read Line counter data -----------------------------------------------------
-    if 
+    if lineCounter.inWaiting():
         
         distanceRecived = lineCounter.readline()
         print('Distance: ' + str(distanceRecived[0:len(distanceRecived)-2]))
