32d2e64ce4ff645a12fadb5af097b066d924716d
Jacob Anderson <jaanderson@fortlewis.edu>
2017-03-18 21:31:09 -0600

    Update Serial Coms

--- SerialComs.py
+++ SerialComs.py
@@ -3,15 +3,13 @@
 import time
 import serial
 
-#print(time.localtime())
+
 
 # Open the serial port for nema sentances ----------------------------------------
 dataPort = serial.Serial("/dev/ttyUSB1",baudrate = 19200)
-print("Data Port: " + dataPort.name)
 
 # Open the serial port with the line counter ----------------------------------------
 lineCounter = serial.Serial("/dev/ttyUSB0",baudrate = 115200, timeout = 3.0)
-print("Line Counter Port: " + lineCounter.name)
 
 
 state = ''
@@ -19,17 +17,15 @@ location = ''
 data = ''
 distance = ''
 
-dataRecived = ''
-distanceRecived = ''
-
 print("Main loop")
 
 while True:
 
     # Read nema data from ROV ------------------------------------------------
     if dataPort.inWaiting():
-        
         dataRecived = dataPort.readline()
+    elif lineCounter.inWaiting():
+        dataRecived = lineCounter.readline()
         
         if dataRecived[0:5] == bytes('$CARE'.encode('utf-8')):
             state = str(dataRecived[7:len(dataRecived)-2])
@@ -47,7 +43,7 @@ while True:
         
 
     # Read Line counter data -----------------------------------------------------
-    if lineCounter.inWaiting():
+    if 
         
         distanceRecived = lineCounter.readline()
         print('Distance: ' + str(distanceRecived[0:len(distanceRecived)-2]))
